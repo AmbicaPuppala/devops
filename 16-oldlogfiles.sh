29cf87847164b393b97a4d31cabd256ab612fbf3
Ambica <mukeshpuppala28@gmail.com>
2025-03-02 05:34:10 -0800

    new script added

--- 16-oldlogfiles.sh
+++ 16-oldlogfiles.sh
@@ -26,3 +26,5 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR . -name "*.log" -mtime +14)
 echo "Files to delete: $FILES_TO_DELETE" 
+
+while 
