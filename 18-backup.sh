844ef318a603b3f66533a46846aa48469d06a777
Ambica <mukeshpuppala28@gmail.com>
2025-03-03 03:07:16 -0800

    new script added

---
+++ 18-backup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR="/home/ec2-user/app-logs"
+DAYS=$[3:-14]
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+USAGE(){
+    echo -e "Usage: $N sh 18-backup.sh $Y <source-dir> <DEST_DIR> <DAYS(optional)> $N"
+    exit 1
+}
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
+
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
