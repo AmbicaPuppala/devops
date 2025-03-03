bff0a9409f26b9fe08af47022f767ad0bcc82c5c
Ambica <mukeshpuppala28@gmail.com>
2025-03-03 03:26:12 -0800

    new script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -32,7 +32,7 @@ then
 fi
 if [ ! -d $SOURCE_DIR ]
 then
-    echo "$Source_DIR directory does not exist"
+    echo "$Source_DIR Source directory does not exist"
     exit 1
 fi
 if [ ! -d $DEST_DIR ]
