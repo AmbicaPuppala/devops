677e2c24dcaf923ee06f96d71661d5fa2a2b4778
Ambica <mukeshpuppala28@gmail.com>
2025-03-03 04:30:02 -0800

    new script added

--- 18-backup.sh
+++ 18-backup.sh
@@ -13,15 +13,6 @@ LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE $N"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-}
 USAGE(){
     echo -e "Usage: $N sh 18-backup.sh $Y <source-dir> <DEST_DIR> <DAYS(optional)> $N"
     exit 1
@@ -53,6 +44,20 @@ then
     echo "Files to backup: $FILES"
     ZIP_FILE="$DEST_DIR/backup-$TIMESTAMP.ZIP"
     find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
+    if [ -f "$ZIP_FILE" ]
+    then
+        echo "Backup file created: $ZIP_FILE"
+        while read -r filepath
+        do
+             echo "Deleting file: $filepath"
+             rm -rf $filepath
+             echo "deleted file: $filepath"
+
+         done <<< $FILES
+    else
+         echo "Backup file creation failed"
+         exit 1
+    fi    
  else
     echo "No files older than $DAYS to backup"
 fi    
