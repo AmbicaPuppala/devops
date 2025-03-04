2bdf592069fcb2bbc8a17e6b516960fa0e510f11
Ambica <mukeshpuppala28@gmail.com>
2025-03-04 10:55:55 -0800

    new script added

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 DISK_USAGE=$(df -h | grep xfs)
-DISK_TRESHHOLD=10
+DISK_TRESHHOLD=5
+MSG=""
 while read -r line
 do
     echo $line
     USAGE=$(echo $line | awk '{print $5}' | cut -d "%" -f1)
-    if [ $USAGE -gt $DISK_TRESHHOLD ]
+    PARTITION=$(echo $line | awk '{print $NF}')
+    if [ $USAGE -ge $DISK_TRESHHOLD ]
     then
         echo "Disk usage is above $DISK_TRESHHOLD"
     fi
