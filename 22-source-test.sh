fd00f0442b319be0628632d64aa8269d77fa6a48
Ambica <mukeshpuppala28@gmail.com>
2025-03-05 06:15:23 -0800

    script added

---
+++ 22-source-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+souce ./22-source-test.sh
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+FILES_TO_DELETE=$(find $SOURCE_DIR . -name "*.log" -mtime +14)
+echo "Files to delete: $FILES_TO_DELETE" 
+
+ while read -r file
+ do
+  echo "Deleting file: $file"
+    rm -rf $file
+
+ done <<< $FILES_TO_DELETE
